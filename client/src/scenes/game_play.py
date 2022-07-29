1a5addfdafc8d1a2bee1cdbd6210524bb6d77c57
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2022-07-30 00:42:42 +0530

    removed back_btn from game_play scene

--- client/src/scenes/game_play.py
+++ client/src/scenes/game_play.py
@@ -22,32 +22,10 @@ class gamePlay(Scene):
 
         self.level = Level(self.screen)
 
-        self.back_btn = Button(
-            50,
-            50,
-            80,
-            30,
-            "Go Back",
-            30,
-            pygame.Color(12, 43, 64),
-            None,
-            lambda: switch_scene("Menu"),
-            self.screen,
-        )
-
     def render(self):
-        # Fill screen black
-        self.screen.fill("black")
-
-        self.back_btn.render()
-
         self.level.render()
 
-        # pygame final display renderer
-        pygame.display.update()
-
     def update(self, events_list):
-        self.back_btn.update(events_list)
         self.level.update(events_list)
         if self.level.death() == True:
             # self.level = Level(self.screen)
